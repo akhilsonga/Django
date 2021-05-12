42ec711ee735ff2cf0a83ba79bfcbf4f06b74d33
akhilsonga <81876214+akhilsonga@users.noreply.github.com>
2021-05-12 22:54:51 +0530

    Create views.py

---
+++ shop/views.py
@@ -0,0 +1,29 @@
+#shop/views
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Products 
+from math import ceil
+def index(request):
+
+    # products = Products.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Products.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Products.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
+
+def about(request):
+    return render(request,"shop/about.html")
